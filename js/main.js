1721ad31bce3045c9eb5b855b9d1b306ee04a469
Karla Falcão <kpsf@cin.ufpe.br>
2016-11-28 13:54:43 -0300

    Fixed normailize to work with new loaded data

--- js/main.js
+++ js/main.js
@@ -1,21 +1,30 @@
 var algorithms = ['pmx1', 'pmx2', 'pmx3', 'pmx4', 'pmx5', 'pmx6'];
+var testeQty = 10;
 var selectedAlgorithm = '0';
 !function(){
     //boxplots functions factory
     var dataset;
 
-    function subscribeSelection(){
+    // Object -> Object
+    var parseInfo = R.map(parseFloat);
+    var parseFitness = R.map(parseFloat, R.prop('fitness'));
+
+
+    function subscribeSelection () {
         var selVal = document.getElementById('alg-select');
+
         selVal.addEventListener('change', function(e) {
             selectedAlgorithm = this.value;
+
             boxPlots.remove();
+
             boxPlots.normalizeData(dataset[selectedAlgorithm]);
             boxPlots.render();
 
-        })
+        });
     }
 
-    function loadCSV(url){
+    function loadCSV (url) {
         return new Promise(function(resolve, reject){
             d3.csv(url, function(data) {
                 resolve(data);
@@ -24,43 +33,45 @@ var selectedAlgorithm = '0';
     }
 
     function loadDatasetForAlgorithm(alg) {
-        var infoData = Array(10).fill().map(function (_, i) { return loadCSV('dados/'+ alg + '/info'+ i +'.csv')});
-        var gensData = Array(10).fill().map(function (_, i) { return loadCSV('dados/'+ alg + '/teste'+ i +'.csv')});
+
+        var infoData = loadCSV('dados/'+ alg + '/info.csv');
+        var gensData = loadCSV('dados/'+ alg + '/teste.csv');
 
         return Promise.all([
-            Promise.all(infoData),
-            Promise.all(gensData)]
-        ).then(function(data) {
+            infoData,
+            gensData
+        ]).then(function(data) {
             infoData = data[0];
             gensData = data[1];
 
-            var dataset = Array(10).fill().map(function (_, testIndex) {
+            var dataset = Array(testeQty).fill().map(function (_, testIndex) {
                 var item = {};
 
-                // set info attr
-                item.info = {};
-                var info = infoData[testIndex];
-
-                info.forEach(function (feature) {
-                    var attr = feature[info.columns[0]];
-                    var value = feature[info.columns[1]];
-                    item.info[attr] = attr === 'fitmedio' ? parseFloat(value) : parseInt(value);
-                });
+                // normalize info data
+                
+                item.info = parseInfo( infoData[testIndex] );
 
                 //
                 var population = item.info.populacao;
-                var gens = gensData[testIndex];
+                var genQty = item.info.gen;
+
+                item.fitness = [];
                 item.variances = [];
-                item.gens = Array(parseInt(gens.length/population)).fill().map(function(_, i) {
-                    var iteration = gens.slice(i*population, (i+1)*population).map(function(sample){
-                        sample.fitness = parseFloat(sample.fitness);
-                        return sample;
-                    });
 
-                    item.variances[i] =  d3.variance(iteration.map(function(sample){ return sample.fitness; }));
+                var gensInTest = R.slice(0, genQty * population, gensData);
+                gensData = R.remove(0, genQty * population, gensData);
+                
+                item.gens = Array(genQty).fill().map(function(_, i) {
+                    var gen = R.slice(0, population, gensInTest);
+                    gensInTest = R.remove(0, population, gensInTest);
 
-                    return iteration;
+                    item.fitness[i] = gen.map(function(sample){ return parseFloat(sample.fitness); });
+                    item.variances[i] =  d3.variance( item.fitness[i] );
+
+                    return gen;
                 });
+
+                console.assert(item.gens.length === genQty, 'Generation qty is not correct!!');
 
                 return item;
             });
@@ -85,7 +96,6 @@ var selectedAlgorithm = '0';
         boxPlots.render();
 
         pcaPlots.render();
-
 
         //#end
     }
