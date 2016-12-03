2d4a9d5e0fc6f2e54215ee16516ae0013be821be
Karla Falcão <kpsf@cin.ufpe.br>
2016-12-03 16:09:29 -0300

    Put labes for bars
    Changed bars colorscheme

--- js/bars.js
+++ js/bars.js
@@ -6,8 +6,8 @@ var barsPlot = function (svgContainerId) {
         .attr("width", 960)
         .attr("height", 500);
 
-    var margin = {top: 20, right: 20, bottom: 110, left: 40},
-        margin2 = {top: 430, right: 20, bottom: 30, left: 40},
+    var margin = {top: 20, right: 120, bottom: 110, left: 40},
+        margin2 = {top: 430, right: 120, bottom: 30, left: 40},
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
         height2 = +svg.attr("height") - margin2.top - margin2.bottom; //40px
@@ -15,7 +15,19 @@ var barsPlot = function (svgContainerId) {
     var parseDate = d3.timeParse("%d/%m/%Y");
 
     var colour = d3.scaleOrdinal()
-                .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+                .range([
+                    '#993404',
+                    '#d95f0e',
+                    '#fe9929',
+                    '#fed98e'
+                ]);
+
+    var counts = [
+        "< 0.25",
+        ">= 0.25 e < 0.50",
+        ">= 0.50 e < 1",
+        "== 1",
+    ];
 
     var x = d3.scaleLinear().range([0, width]),
         x2 = d3.scaleLinear().range([0, width]),
@@ -46,21 +58,6 @@ var barsPlot = function (svgContainerId) {
         .attr("class", "context")
         .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-    function parse(d) {
-        d.date = new Date(d.date);
-        // var value = { date: parseDate(d.date) };
-        // var y0 = 0;
-        // value.counts = ["count", "count2", "count3"].map(function(name) {
-        //     return { 
-        //         name: name,
-        //         y0: y0,
-                // y1: y0 += +d[name]
-        //     };
-        // });
-        // value.total = value.counts[value.counts.length - 1].y1;
-        return d;
-    }
-
      var indivOccurrences = {
         // '[5, 3, 6, 0, 2, 4, 1, 7]' : {
         //     'Test1Gen0': 3,
@@ -72,7 +69,6 @@ var barsPlot = function (svgContainerId) {
         var teste1 = data[0];
 
         var allGens = data.reduce(function(prev, cur, sampleIndex) {
-           
 
            var gensData = cur.gens.map(function(gen, genIndex) {
             
@@ -84,8 +80,8 @@ var barsPlot = function (svgContainerId) {
                 var freq = {
                     count1: gen.filter(function(indiv){ return indiv.fitness < 0.25; }).length,
                     count2: gen.filter(function(indiv){ return indiv.fitness >= 0.25 && indiv.fitness < 0.50; }).length,
-                    count3: gen.filter(function(indiv){ return indiv.fitness >= 0.50 && indiv.fitness < 0.75; }).length,
-                    count4: gen.filter(function(indiv){ return indiv.fitness >= 0.75;}).length,
+                    count3: gen.filter(function(indiv){ return indiv.fitness >= 0.50 && indiv.fitness < 1; }).length,
+                    count4: gen.filter(function(indiv){ return indiv.fitness === 1;}).length,
                 };
 
                 var y0 = 0;
@@ -105,13 +101,12 @@ var barsPlot = function (svgContainerId) {
         return allGens;
     }
 
-    var render = function(data){
-
+    var render = function(data) {
         x.domain([0, data.length - 1]);
         y.domain([0, d3.max(data, function(d) { return d.total; })]);
         x2.domain(x.domain());
         y2.domain(y.domain());
-        colour.domain(d3.keys(data[0]));
+        colour.domain(counts);
         
         // draw the bars
         focus.append("g")
@@ -128,37 +123,59 @@ var barsPlot = function (svgContainerId) {
         .enter()
         .append("rect")
         .attr("class", "bar")
-        .attr("width", 6)
+        .attr("width", 10)
         .attr("y", function(d) { return y(d.y1); })
         .attr("height", function(d) { return y(d.y0) - y(d.y1); })
         .style("fill", function(d) { return colour(d.name); });
 
 
-        focus.append("g")
+        var xAxisElem = focus.append("g")
             .attr("class", "axis axis--x")
             .attr("transform", "translate(0," + height + ")")
             .call(xAxis.tickValues([0,250,500,750,1000,1250,1500,1750,2000,2250])
 	            	.tickFormat(function (d,i){
 	            		return ["Teste "+(i+1)];
 	            	}));
+        
+        xAxisElem.append('text')
+        .attrs({
+            transform: 'translate(-30, ' + -(height/2) +') rotate(-90)',
+            y: 6,
+            'font-size' : '1em',
+            fill: '#000'
+        })
+        .text('Indivíduos');
 
         focus.append("g")
             .attr("class", "axis axis--y")
             .call(yAxis);
 
-        // draw bars
+        // draw overview bars
         context.append("g")
             .attr("class", "bars")
             .attr('clip-path', 'url(#clip)')
             .selectAll(".bar")
             .data(data)
         .enter()
+        .append("g")
+        .attr("class", "bar stack")
+        .attr("transform", function(d, i) { return "translate(" + x2(i) + ",0)"; })
+            .selectAll("rect")
+            .data(function(d) { return d.counts; })
+        .enter()
         .append("rect")
         .attr("class", "bar")
-        .attr("x", function(d, i) { return x2(i) - 3; })
-        .attr("width", 6)
-        .attr("y", function(d) { return y2(d.total); })
-        .attr("height", function(d) { return height2 - y2(d.total); });
+        .attr("width", 10)
+        .attr("y", function(d) { return y2(d.y1); })
+        .attr("height", function(d) { return y2(d.y0) - y2(d.y1); })
+        .style("fill", function(d) { return colour(d.name); });
+
+        // .append("rect")
+        // .attr("class", "bar")
+        // .attr("x", function(d, i) { return x2(i) - 3; })
+        // .attr("width", 6)
+        // .attr("y", function(d) { return y2(d.total); })
+        // .attr("height", function(d) { return height2 - y2(d.total); })
 
         context.append("g")
             .attr("class", "axis axis--x")
@@ -171,6 +188,8 @@ var barsPlot = function (svgContainerId) {
         context.append("g")
             .attr("class", "brush")
             .call(brush);
+
+        showColorLabels();
     };
 
     function brushed() {
@@ -204,6 +223,91 @@ var barsPlot = function (svgContainerId) {
                         
                     }));
         }
+    }
+
+    function showColorLabels() {
+
+        labels = svg.append('g')
+            .attr('class', 'labels')
+            .attr('transform', 'translate('+ (width + margin.left + 10) + ' , 0)');
+
+        labels
+            .append('text')
+            .text('Intervalo de fitness por cores')
+            .attrs({
+                x: 0,
+                y: 0,
+                'font-size': '11px',
+                'font-weight': 'bold',
+                'transform': 'translate(0, -5)'
+            });
+
+        var labelsGroup = labels.selectAll('g')
+            .data(counts);
+
+        // Enter...
+        var labelsGroupAdd = labelsGroup
+            .enter()
+            .append('g')
+            .attr('class', 'line-group');
+
+        // Update
+        var labelsGroupUpdate = labelsGroup
+            .transition()
+            .duration(500);
+
+        labelsGroup
+            .exit()
+            .remove();
+        
+        labelsGroupAdd
+            .append('rect')
+            .attr('height', 10)
+            .attr('width', 10)
+            .attr('y', function (d, i) {
+                return i * 17;
+            })
+            .attr('fill', function (d, i) {
+                return colour('count'+(i+1));
+            });
+
+        labelsGroupAdd
+            .append('text')
+            .text(function (d, i) {
+                return d;
+            })
+            .attrs({
+                x: 12,
+                y: function (d, i) {
+                    return (i + 1) * 16;
+                },
+                'font-size': '11px',
+                'transform': 'translate(0, -5)'
+            });
+
+        // labelsGroupUpdate
+        //     .select('rect')
+        //     .attr('height', 10)
+        //     .attr('width', 10)
+        //     .attr('y', function (d, i) {
+        //         return i * 17;
+        //     })
+        //     .attr('fill', function (d, i) {
+        //         return colour('count'+(i+1));
+        //     });
+
+        // labelsGroupUpdate
+        //     .select('text')
+        //     .text(function (d, i) {
+        //         return d;
+        //     })
+        //     .attr('y', function (d, i) {
+        //         return (i + 1) * 17;
+        //     })
+        //     .attr('x', 12)
+        //     .attrs({
+        //         'transform': 'translate(0, -5)'
+        //     });
     }
 
     return {
